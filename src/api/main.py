db7c1f01ffc516f143efef663b847ea8c42b5b84
maro114510 <atumaro114510@icloud.com>
2022-09-19 16:54:17 +0900

    主処理コンテナの設定

---
+++ src/api/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def hello():
+    return {"message": "hello world!"}
+#--- EoF ---
+
+
+# End of Script
