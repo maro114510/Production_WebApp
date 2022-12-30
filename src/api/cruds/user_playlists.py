7c822aca4619b90feb2967bb53a1e03ce72f991f
maro114510 <atumaro1144510@icloud.com>
2022-12-30 15:53:34 +0900

    user_playlistsの実装を完了

--- src/api/cruds/user_playlists.py
+++ src/api/cruds/user_playlists.py
@@ -22,6 +22,120 @@ class UserPlaylists():
 		self.conn = ins.Connector()
 	#--- EoF ---
 
+	def get_all_user_playlists_full_info( self ):
+		cur = self.conn.cursor( cursor_factory=RealDictCursor )
+		sql = self.select_all_sql()
+		try:
+			cur.execute( sql )
+			results = cur.fetchall()
+			self.conn.commit()
+			print( "SELECT OK" )
+			return results
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
+	def get_one_user_playlists_full_info( self, uid ):
+		cur = self.conn.cursor( cursor_factory=RealDictCursor )
+		sql = self.select_by_uid_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					uid,
+				)
+			)
+			results = cur.fetchmany()
+			self.conn.commit()
+			print( "SELECT OK" )
+			return results
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
+	def get_one_user_playlists_d_info( self, uid ):
+		cur = self.conn.cursor( cursor_factory=RealDictCursor )
+		sql = self.select_d_by_uid_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					uid,
+				)
+			)
+			results = cur.fetchmany()
+			self.conn.commit()
+			print( "SELECT OK" )
+			return results
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
+	def user_playlists_insert( self, uid, p_org_id ):
+		cur = self.conn.cursor()
+		sql = self.insert_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					uid,
+					p_org_id
+				)
+			)
+			self.conn.commit()
+			print( "INSERT OK" )
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
+	def delete_user_playlist( self, uid, p_org_id ):
+		cur = self.conn.cursor()
+		sql = self.delete_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					uid,
+					p_org_id,
+				)
+			)
+			self.conn.commit()
+			print( "DELETE OK" )
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+		return 0
+	#--- EoF ---
+
+	def update_user_playlist( self, uid, p_org_id ):
+		cur = self.conn.cursor()
+		sql = self.update_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					uid,
+					p_org_id,
+				)
+			)
+			self.conn.commit()
+			print( "DELETE OK" )
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+		return 0
+	#--- EoF ---
+
 	def execute( self ):
 		print( "OK" )
 	#--- EoF ---
@@ -29,6 +143,91 @@ class UserPlaylists():
 	def main( self, argc, argv ):
 		self.execute()
 		return 0
+	#--- EoF ---
+
+	def insert_sql( self ):
+		sql = """
+		INSERT INTO t_user_playlists (
+			uid,
+			p_org_id
+		) VALUES (
+			%s,
+			%s
+		);
+		"""
+		return sql
+	#--- EoF ---
+
+	def select_all_sql( self ):
+		sql = """
+		SELECT
+			*
+		FROM
+			t_user_playlists;
+		"""
+		return sql
+	#--- EoF ---
+
+	def select_by_uid_sql( self ):
+		sql = """
+			SELECT
+				*
+			FROM
+				t_user_playlists
+			WHERE
+				uid = %s
+			AND
+				flag = true
+			;
+		"""
+		return sql
+	#--- EoF ---
+
+	def select_d_by_uid_sql( self ):
+		sql = """
+			SELECT
+				*
+			FROM
+				t_user_playlists
+			WHERE
+				uid = %s
+			AND
+				flag = false
+			;
+		"""
+		return sql
+	#--- EoF ---
+
+	def delete_sql( self ):
+		sql = """
+		UPDATE
+			t_user_playlists
+		SET
+			flag = false,
+			modified_at = CURRENT_TIMESTAMP
+		WHERE
+			uid = %s
+		AND
+			p_org_id = %s
+		;
+		"""
+		return sql
+	#--- EoF ---
+
+	def update_sql( self ):
+		sql = """
+		UPDATE
+			t_user_playlists
+		SET
+			flag = true,
+			modified_at = CURRENT_TIMESTAMP
+		WHERE
+			uid = %s
+		AND
+			p_org_id = %s
+		;
+		"""
+		return sql
 	#--- EoF ---
 #--- UserPlaylists ---
 
--- src/api/routers/user_playlists.py
+++ src/api/routers/user_playlists.py
@@ -14,5 +14,78 @@ router = APIRouter()
 
 @router.get( "/user_playlists", tags=[ "UserPlaylists" ] )
 async def list_user_playlists() -> List[ dict ]:
+	return ins.get_all_user_playlists_full_info()
+#--- EoF ---
+
+@router.get( "/user_playlists/user_playlists", tags=[ "UserPlaylists" ] )
+async def get_user_playlists_info( uid: int ):
+	return ins.get_one_user_playlists_full_info( uid )
+#--- EoF ---
+
+@router.get( "/user_playlists/d/user_playlists", tags=[ "UserPlaylists" ] )
+async def get_d_user_playlists_info( uid: int ):
+	return ins.get_one_user_playlists_d_info( uid )
+#--- EoF ---
+
+
+@router.post( "/user_playlists", tags=[ "UserPlaylists" ] )
+async def create_user_playlists(
+	uid: int,
+	p_org_id: str
+):
+	try:
+		ins.user_playlists_insert(
+			uid,
+			p_org_id
+		)
+	except Exception as e:
+		print( "%s" % ( [e.args, ] ), file=sys.stderr )
+		raise HTTPException(
+			status_code=404,
+			detail="Either the user's email address or the user's password is duplicated. Please change it.",
+		)
+	#-- except
 	return 0
 #--- EoF ---
+
+
+@router.delete( "/user_playlists", tags=[ "UserPlaylists" ] )
+async def delete_user_playlists(
+	uid: int,
+	p_org_id: str
+) -> int:
+	try:
+		ins.delete_user_playlist( uid, p_org_id )
+	except Exception as e:
+		print( "%s" % ( [e.args, ] ), file=sys.stderr )
+		raise HTTPException(
+			status_code=404,
+			detail="Sorry, Try again.",
+		)
+	#-- except
+	return 0
+#--- EoF ---
+
+
+@router.put( "/user_playlists", tags=[ "UserPlaylists" ] )
+async def update_user_playlists(
+	uid: int,
+	p_org_id
+):
+	try:
+		ins.update_user_playlist(
+			uid,
+			p_org_id
+		)
+	except Exception as e:
+		print( "%s" % ( [e.args, ] ), file=sys.stderr )
+		raise HTTPException(
+			status_code=404,
+			detail="Sorry, Try again.",
+		)
+	#-- except
+	return 0
+#--- EoF ---
+
+
+# End of Script
