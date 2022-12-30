eb8247f7528118bdd2163c76c80a8d8a4b12c25d
maro114510 <atumaro1144510@icloud.com>
2022-12-31 00:05:32 +0900

    特になし

---
+++ src/api/cron/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import sys
+from pathlib import Path
+from psycopg2.extras import RealDictCursor
+
+sys.path.append(
+	str(
+		Path(
+			__file__
+		).resolve().parent.parent.parent
+	)
+)
+
+from api.db.conn import Connector
+
+
+class Cron():
+	def __init__( self ):
+		ins = Connector()
+		self.conn = ins.Connector()
+	#--- EoF ---
+
+	def execute( self ):
+		print( "OK" )
+	#--- EoF ---
+
+	def main( self, argc, argv ):
+		self.execute()
+		return 0
+	#--- EoF ---
+#--- Cron ---
+
+
+# Entry Point
+
+if __name__ == "__main__":
+	ins = Cron()
+	sys.exit( ins.main( len( sys.argv ), sys.argv ) )
+#-- if
+
+
+
+# End of Script
