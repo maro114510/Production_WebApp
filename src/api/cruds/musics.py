55197140cd672761e2b897be739ee1146aec711d
maro114510 <atumaro1144510@icloud.com>
2022-12-30 13:20:10 +0900

    バルクインサート前

--- src/api/cruds/musics.py
+++ src/api/cruds/musics.py
@@ -22,6 +22,67 @@ class Musics():
 		self.conn = ins.Connector()
 	#--- EoF ---
 
+	def get_all_musics_full_info( self ):
+		cur = self.conn.cursor( cursor_factory=RealDictCursor )
+		sql = self.select_all_sql()
+		try:
+			cur.execute( sql )
+			results = cur.fetchall()
+			self.conn.commit()
+			print( "SELECT OK" )
+			return results
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
+	def get_one_music_info( self, m_org_id ):
+		cur = self.conn.cursor( cursor_factory=RealDictCursor )
+		sql = self.select_one_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					m_org_id,
+				)
+			)
+			result = cur.fetchone()
+			self.conn.commit()
+			print( "SELECT OK" )
+			return result
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
+	def music_insert_one( self, music_name, m_org_id ):
+		cur = self.conn.cursor()
+		sql = self.insert_one_sql()
+		try:
+			cur.execute(
+				sql,
+				(
+					music_name,
+					m_org_id,
+					m_org_id,
+				)
+			)
+			self.conn.commit()
+
+			if cur.rowcount != 0:
+				print( "INSERT OK" )
+			#-- if
+			else:
+				print( "DUPLICATED" )
+			#-- else
+		except Exception as e:
+			self.conn.rollback()
+			raise e
+		#-- except
+	#--- EoF ---
+
 	def execute( self ):
 		print( "OK" )
 	#--- EoF ---
@@ -29,6 +90,45 @@ class Musics():
 	def main( self, argc, argv ):
 		self.execute()
 		return 0
+	#--- EoF ---
+
+	def insert_one_sql( self ):
+		sql = """
+		INSERT INTO t_musics (
+			music_name,
+			m_org_id
+		)
+		SELECT
+			%s,
+			%s
+			WHERE NOT EXISTS (
+				SELECT 1 FROM t_musics WHERE m_org_id = %s
+			)
+		;
+		"""
+		return sql
+	#--- EoF ---
+
+	def select_all_sql( self ):
+		sql = """
+		SELECT
+			*
+		FROM
+			t_musics;
+		"""
+		return sql
+	#--- EoF ---
+
+	def select_one_sql( self ):
+		sql = """
+		SELECT
+			*
+		FROM
+			t_musics
+		WHERE
+			m_org_id = %s;
+		"""
+		return sql
 	#--- EoF ---
 #--- Musics ---
 
--- src/api/routers/musics.py
+++ src/api/routers/musics.py
@@ -13,10 +13,53 @@ router = APIRouter()
 
 @router.get( "/musics", tags=[ "Musics" ] )
 async def list_musics() -> List[ dict ]:
+	return ins.get_all_musics_full_info()
+#--- EoF ---
+
+@router.get( "/musics/music", tags=[ "Musics" ] )
+async def get_music_info( m_org_id: str ):
+	return ins.get_one_music_info( m_org_id )
+#--- EoF ---
+
+@router.post( "/musics", tags=[ "Musics" ] )
+async def create_music(
+	music_name: str,
+	m_org_id: str
+):
+	try:
+		ins.music_insert_one(
+			music_name,
+			m_org_id,
+		)
+	except Exception as e:
+		print( "%s" % ( [e.args, ] ), file=sys.stderr )
+		raise HTTPException(
+			status_code=404,
+			detail="",
+		)
+	#-- except
 	return 0
 #--- EoF ---
 
-
+@router.post( "/musics/bulk", tags=[ "Musics" ] )
+async def create_musics(
+	musics: list
+):
+	try:
+		# ins.music_insert_one(
+		# 	music_name,
+		# 	m_org_id,
+		# )
+		pass
+	except Exception as e:
+		print( "%s" % ( [e.args, ] ), file=sys.stderr )
+		raise HTTPException(
+			status_code=404,
+			detail="",
+		)
+	#-- except
+	return 0
+#--- EoF ---
 
 
 # End of Script
